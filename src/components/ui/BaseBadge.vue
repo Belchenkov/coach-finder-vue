3b030651f2d4a41efe0c440ffcc66e23859dbb95
Aleksey Belchenkov <u608110@gmail.com>
2020-11-15 21:31:01 +0300

    Base UI Components

---
+++ src/components/ui/BaseBadge.vue
@@ -0,0 +1,46 @@
+<template>
+  <span class="badge" :class="type">
+    {{ text }}
+  </span>
+</template>
+
+<script>
+export default {
+  name: "BaseBadge",
+  props: [
+      'type',
+      'title'
+  ],
+  computed: {
+    text() {
+      return this.title.toUpperCase();
+    }
+  }
+}
+</script>
+
+<style scoped>
+.badge {
+  background-color: #ccc;
+  color: #252525;
+  border-radius: 30px;
+  padding: 0.5rem 1.5rem;
+  display: inline-block;
+  margin: 1rem .5rem 1rem 0;
+}
+
+.frontend {
+  background-color: #d75e0d;
+  color: white;
+}
+
+.backend {
+  background-color: #a55b2a;
+  color: white;
+}
+
+.career {
+  background-color: #725039;
+  color: white;
+}
+</style>
---
+++ src/components/ui/BaseButton.vue
@@ -0,0 +1,75 @@
+<template>
+  <button v-if="!link" :class="mode">
+    <slot></slot>
+  </button>
+  <router-link v-else :to="to" :class="mode">
+    <slot></slot>
+  </router-link>
+</template>
+
+<script>
+export default {
+  name: "BaseButton",
+  props: {
+    link: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    to: {
+      type: String,
+      required: false,
+      default: '/'
+    },
+    mode: {
+      type: String,
+      required: false,
+      default: null
+    }
+  }
+}
+</script>
+
+<style scoped>
+  button,
+  a {
+    text-decoration: none;
+    padding: 0.75rem 1.5rem;
+    font: inherit;
+    background-color: #2f6f71;
+    border: 1px solid #2f6f71;
+    color: white;
+    cursor: pointer;
+    border-radius: 30px;
+    margin-right: 0.5rem;
+    display: inline-block;
+    font-weight: bold;
+  }
+
+  a:hover,
+  a:active,
+  button:hover,
+  button:active {
+    background-color: #2f6f71;
+    border-color: #2f6f71;
+  }
+
+  .flat {
+    background-color: transparent;
+    color: #2f6f71;
+    border: none;
+  }
+
+  .outline {
+    background-color: transparent;
+    border-color: #2f6f71;
+    color: #2f6f71;
+  }
+
+  .flat:hover,
+  .flat:active,
+  .outline:hover,
+  .outline:active {
+    background-color: #dff5e8;
+  }
+</style>
