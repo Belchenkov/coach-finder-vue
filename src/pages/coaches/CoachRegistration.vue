2fa514c2221386dfabf21758a9a3e6fd3c7ec203
Aleksey Belchenkov <u608110@gmail.com>
2020-11-19 13:53:53 +0300

    Register Coach Form

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,10 +1,20 @@
 <template>
-  CoachRegistration
+  <section>
+    <base-card>
+      <h2>Register as a coach now!</h2>
+      <CoachForm />
+    </base-card>
+  </section>
 </template>
 
 <script>
+import CoachForm from "@/components/coaches/CoachForm";
+
 export default {
-name: "CoachRegistration"
+  name: "CoachRegistration",
+  components: {
+    CoachForm
+  }
 }
 </script>
 
