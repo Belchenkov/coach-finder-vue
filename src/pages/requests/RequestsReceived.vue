5ed29c1c136707bc0c33d141986eaf706cbaffc7
Aleksey Belchenkov <u608110@gmail.com>
2021-04-20 18:45:26 +0400

    Fetch and Store Requests

--- src/pages/requests/RequestsReceived.vue
+++ src/pages/requests/RequestsReceived.vue
@@ -1,10 +1,22 @@
 <template>
+  <base-dialog
+      :show="!!error"
+      title="An error occurred"
+      @close="handleError"
+  >
+    <p>{{ error }}</p>
+  </base-dialog>
   <section>
     <base-card>
       <header>
         <h2>Requests Received</h2>
       </header>
-      <ul v-if="hasRequests">
+
+      <div v-if="isLoading">
+        <base-spinner></base-spinner>
+      </div>
+
+      <ul v-else-if="hasRequests && !isLoading">
         <request-item
           v-for="req in receivedRequests"
           :key="req.id"
@@ -25,6 +37,12 @@ export default {
   components: {
     RequestItem
   },
+  data() {
+    return {
+      isLoading: false,
+      error: null
+    }
+  },
   computed: {
     receivedRequests() {
       return this.$store.getters['requests/requests'];
@@ -32,6 +50,25 @@ export default {
     hasRequests() {
       return this.$store.getters['requests/hasRequests'];
     }
+  },
+
+  methods: {
+    async loadRequests() {
+      this.isLoading = true;
+      try {
+        await this.$store.dispatch('requests/fetchRequests');
+      } catch (error) {
+        this.error = error.message || 'Something failed!';
+      }
+
+      this.isLoading = false;
+    },
+    handleError() {
+      this.error = null;
+    }
+  },
+  created() {
+    this.loadRequests();
   }
 }
 </script>
